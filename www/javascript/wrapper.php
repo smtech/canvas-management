13fa08520ea52520afce9e7360126458345b8909
Seth Battis <SethBattis@stmarksschool.org>
2013-06-26 12:55:54 -0400

    JavaScript Wrapper
    A wrapper file to let us individually toggle JavaScript modification on and off (one modification per .js file, please!)
    
    Signed-off-by: Seth Battis <SethBattis@stmarksschool.org>

--- www/javascript/wrapper.php
+++ www/javascript/wrapper.php
@@ -1,5 +1,7 @@
 <?php
 
+// a wrapper file to allow us to individually add (and remove) JavaScripted modifications
+
 include_once("discussion-permalinks.js");
 include_once("resources-menu.js");
 include_once('course-calendar.js');
