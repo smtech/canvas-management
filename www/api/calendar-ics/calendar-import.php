c8af606973cab564b7358d1606092c2d9c64ee13
Seth Battis <SethBattis@stmarksschool.org>
2013-08-08 09:04:27 -0400

    Canvas ICS Feed Exposed
    The webcal feed for Canvas courses is now exposed via the Calendar Export page.
    
    iCalCreate has been removed, since we don't actually need to create ICS feeds!
    
    Signed-off-by: Seth Battis <SethBattis@stmarksschool.org>

--- www/api/calendar-ics/calendar-import.php
+++ www/api/calendar-ics/calendar-import.php
@@ -7,6 +7,9 @@ require_once('config.inc.php');
 
 if (isset($_REQUEST['feed'])) {
 	$feed = file_get_contents($_REQUEST['feed']);
+	
+	require_once('./phpicalendar/functions/ical_parser.php');
+	
 } else {
 	displayPage('
 <form action="' . $_SERVER['PHP_SELF'] . '" method="get">
--- www/api/calendar-ics/config.inc.php
+++ www/api/calendar-ics/config.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 define('TOOL_NAME', 'Canvas Calendar &harr; ICS Tool');
-define('TOOL_START_PAGE', $_SERVER['PHP_SELF']); // FIXME: should be index
+define('TOOL_START_PAGE', dirname($_SERVER['PHP_SELF'])); // FIXME: should be index
 
 define('WORKING_DIR', '/var/www-data/canvas/calendar-ics/');
 define('API_CLIENT_ERROR_RETRIES', 2); // how many times to retry requests for which we got client errors that we don't entirely believe
