90cd1abe2837f2e04e3156e26f9639ad7f02f16b
Seth Battis <SethBattis@stmarksschool.org>
2013-08-09 10:14:27 -0400

    Use Defaults
    Since we don't have to define constants for everything in config, we're just going to rely on default values.
    
    Signed-off-by: Seth Battis <SethBattis@stmarksschool.org>

--- www/api/calendar-ics/config.inc.php
+++ www/api/calendar-ics/config.inc.php
@@ -4,6 +4,5 @@ define('TOOL_NAME', 'Canvas Calendar &harr; ICS Tool');
 define('TOOL_START_PAGE', dirname($_SERVER['PHP_SELF'])); // FIXME: should be index
 
 define('WORKING_DIR', '/var/www-data/canvas/calendar-ics/');
-define('API_CLIENT_ERROR_RETRIES', 2); // how many times to retry requests for which we got client errors that we don't entirely believe
 
 ?>
