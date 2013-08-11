9ef1159a20c16d1c08ced1ac2a0df813d562b02d
Seth Battis <SethBattis@stmarksschool.org>
2013-08-11 16:40:09 -0400

    Syncing
    Hourly, daily and weekly syncing are now available options. Choosing one of these options will schedule a crontab job (if it does not already exist) to trigger the scheduled import (via sync.php).
    
    There is room in here to update the form to create custom schedules as well, but I haven't bothered with that yet.
    
    OAuth is becoming increasingly… pertinent (as is the DELETEing of calendar events)...
    
    Signed-off-by: Seth Battis <SethBattis@stmarksschool.org>

---
+++ www/api/calendar-ics/sync.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once('../debug.inc.php');
+define('DEBUGGING', DEBUGGING_GENERAL | DEBUGGING_LOG | DEBUGGING_CANVAS_API);
+
+require_once('.ignore.calendar-ics-authentication.inc.php');
+require_once('config.inc.php');
+
+require_once('../canvas-api.inc.php');
+require_once('../mysql.inc.php');
+require_once('../Pest.php');
+
+require_once('common.inc.php');
+
+$schedulesResponse = mysqlQuery("
+	SELECT *
+		FROM `schedules`
+		WHERE
+			`schedule` = '" . mysqlEscapeString($argv[INDEX_SCHEDULE]) . "'
+		ORDER BY
+			`synced` ASC
+");
+
+$import = new Pest($argv[INDEX_WEB_PATH]);
+while($schedule = $schedulesResponse->fetch_assoc()) {
+	$calendarResponse = mysqlQuery("
+		SELECT *
+			FROM `calendars`
+			WHERE
+				`id` = '{$schedule['calendar']}'
+	");
+	if ($calendar = $calendarResponse->fetch_assoc()) {
+		$import->get(
+			'import', // assumes ../.htaccess with RewriteCond
+			array(
+				'cal' => $calendar['ics_url'],
+				'canvas_url' => $calendar['canvas_url'],
+				'schedule' => $schedule['id']
+			)
+		);
+	}
+}
+
+?>
