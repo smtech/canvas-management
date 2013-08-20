cd9d24971cc3dd3f2db126649e046e400cd44e38
Seth Battis <SethBattis@stmarksschool.org>
2013-08-20 13:30:38 -0400

    Styling, Individual Users
    Removed extraneous 'a' from unframe.php.
    
    Added individual user filtering and optional CSS styling to menu.
    
    Signed-off-by: Seth Battis <SethBattis@stmarksschool.org>

--- www/javascript/resources-menu.js
+++ www/javascript/resources-menu.js
@@ -3,9 +3,13 @@
 // types of user
 var USER_CLASS_STUDENT = 'student';
 var USER_CLASS_FACULTY = 'faculty';
-var userClass = USER_CLASS_STUDENT;
+var USER_CLASS_NO_MENU = 'no-menu';
+
+// default to hiding the menu
+var userClass = USER_CLASS_NO_MENU;
 
 // define your menu here
+var colorStripe = 'background: #ecf6ff';
 var customMenu = {
 	// menu title
 	// can also have a url property, so clicking the menu title sends you somewhere
@@ -14,14 +18,41 @@ var customMenu = {
 	// an array of columns to display (don't go overboard here!)
 	columns: [
 		{
+			// columns can be divided into titled sections
+			sections: [
+				{
+					// title, target, url are all properties of columns
+					title: 'The Center',
+					items: [
+						// each item can have title, subtitle, target and url
+						{title: 'Writing Lab', subtitle: 'in The Center', url: '/courses/495'},
+						{title: 'Mathematics Lab', subtitle: 'in The Center', url: '/courses/494'},
+						{title: 'Resources for Students', subtitle: 'from The Center', url: '/courses/491'},
+						{title: 'Resources for Faculty', subtitle: 'from The Center', url: '/courses/492', userClass: [USER_CLASS_FACULTY]}
+					]
+				},
+				{
+					title: 'Work Process',
+					items: [
+						{title: 'Library', url: 'http://library.stmarksschool.org', subtitle: 'Catalog, Online Resources, References', target: '_blank'},
+						{title: 'Writing Manual', subtitle: 'Strunk &amp; White @ SM'},
+						{title: 'Faculty Resources', subtitle: 'Calendars, Forms, Policies, Guides', url: '/courses/97', userClass: [USER_CLASS_FACULTY]}
+					]
+				}
+			]
+		},
+		{
+			// style: 'optional CSS goes here',
 			sections: [
 				{
 					title: 'Communication &amp; Storage',
+					//style: 'optional CSS goes here',
 					items: [
 						{
 							title: 'Email',
 							subtitle: 'Google Apps for Education',
 							target: '_blank',
+							// style: 'optional CSS goes here',
 							url: 'http://mail.stmarksschool.org'
 						},
 						{
@@ -45,6 +76,7 @@ var customMenu = {
 				},
 				{
 					title: 'On Campus',
+					style: colorStripe,
 					items: [
 						{
 							title: 'Weekend Activities Sign-ups',
@@ -107,6 +139,7 @@ var customMenu = {
 				{
 					title: 'Service Desks',
 					userClass: [USER_CLASS_FACULTY],
+					style: colorStripe,
 					items: [
 						{
 							title: 'Help Desk',
@@ -128,38 +161,41 @@ var customMenu = {
 					]
 				}
 			]
-		},
-		{
-			// columns can be divided into titled sections
-			sections: [
-				{
-					// title, target, url are all properties of columns
-					title: 'The Center',
-					items: [
-						// each item can have title, subtitle, target and url
-						{title: 'Writing Lab', subtitle: 'in The Center', url: '/courses/495'},
-						{title: 'Mathematics Lab', subtitle: 'in The Center', url: '/courses/494'},
-						{title: 'Resources for Students', subtitle: 'from The Center', url: '/courses/491'},
-						{title: 'Resources for Faculty', subtitle: 'from The Center', url: '/courses/492', userClass: [USER_CLASS_FACULTY]}
-					]
-				},
-				{
-					title: 'Work Process',
-					items: [
-						{title: 'Library', url: 'http://library.stmarksschool.org', subtitle: 'Catalog, Online Resources, References', target: '_blank'},
-						{title: 'Writing Manual', subtitle: 'Strunk &amp; White @ SM'},
-						{title: 'Faculty Resources', subtitle: 'Calendars, Forms, Policies, Guides', url: '/courses/97', userClass: [USER_CLASS_FACULTY]}
-					]
-				}
-			]
 		}
 	]
 };
 
 // if the Faculty Resources item is in the Courses menu, this person is faculty and should get faculty-specific resources
-var facultyCourseId = '956'; // 97 on live server, 956 on test (for now)
+var facultyCourseId = '97'; // 97 on live server, 956 on test (for now)
+var testers = [
+	'Seth Battis',
+	'Brian Lester',
+	'Carlos Collazo',
+	'Ken Griffin',
+	'Brian Fedy',
+	'Michael Wirtz',
+	'Heather Laudani',
+	// adding new faculty so they never don't have the Resources Menu
+	'Brian Burbank',
+	'Caitlin Jones',
+	'Eric Monheim',
+	'Elizabeth Ramirez',
+	'Elana Snow',
+	'James Wallace',
+	'Channing Weymouth',
+	'Christopher Young'
+];
 function setUserClass() {
 	var i;
+	
+	// check user name to identify testers
+	var userName = document.getElementsByClassName('user_long_name')[0].innerText;
+	// if not an individually allowed user, don't process them!
+	if (testers.indexOf(userName) === -1) {
+		return;
+	}
+	
+	// check for Faculty Resources course to identify USER_CLASS_FACULTY
 	var coursesMenu = document.getElementById('menu_enrollments').childNodes[3].childNodes;
 	for (i = 1; i < coursesMenu.length; i += 2) {
 		if (coursesMenu[i] instanceof HTMLLIElement && coursesMenu[i].getAttribute('data-id') === facultyCourseId) {
@@ -184,15 +220,15 @@ function appendMenu(m) {
 	for(i = 0; i < m.columns.length; i++) {
 		if (!m.columns[i].userClass || m.columns[i].userClass.indexOf(userClass) > -1)
 		{
-			html += '<td class="menu-item-drop-column">';
+			html += '<td class="menu-item-drop-column"' + (m.columns[i].style !== undefined ? ' style="' + m.columns[i].style + '"': '') + '>';
 			for (j = 0; j < m.columns[i].sections.length; j++) {
 				if (!m.columns[i].sections[j].userClass || m.columns[i].sections[j].userClass.indexOf(userClass) > -1) {
-					html += (m.columns[i].sections[j].title !== undefined ? '<span class="menu-item-heading">' + m.columns[i].sections[j].title + '</span>' : '');
-					html += '<ul class="menu-item-drop-column-list">';
+					html += (m.columns[i].sections[j].title !== undefined ? '<span class="menu-item-heading"' + (m.columns[i].sections[j].style !== undefined ? ' style="' + m.columns[i].sections[j].style + '"' : '') + '>' + m.columns[i].sections[j].title + '</span>' : '');
+					html += '<ul class="menu-item-drop-column-list"' + (m.columns[i].sections[j].style !== undefined ? ' style="' + m.columns[i].sections[j].style + '"' : '') + '>';
 		
 					for (k = 0; k < m.columns[i].sections[j].items.length; k++) {
 						if (!m.columns[i].sections[j].items[k].userClass || m.columns[i].sections[j].items[k].userClass.indexOf(userClass) > -1) {
-							html += '<li><a' + (m.columns[i].sections[j].items[k].target !== undefined ? ' target="' + m.columns[i].sections[j].items[k].target + '"' : '') + (m.columns[i].sections[j].items[k].url !== undefined ? ' href="' + m.columns[i].sections[j].items[k].url + '"' : '') + '><span class="name ellipsis">' + m.columns[i].sections[j].items[k].title + '</span>' + (m.columns[i].sections[j].items[k].subtitle !== undefined ? '<span class="subtitle">' + m.columns[i].sections[j].items[k].subtitle + '</span>' : '') + '</a></li>';
+							html += '<li' + (m.columns[i].sections[j].items[k].style !== undefined ? ' style="' + m.columns[i].sections[j].items[k].style + '"' : '') + '><a' + (m.columns[i].sections[j].items[k].target !== undefined ? ' target="' + m.columns[i].sections[j].items[k].target + '"' : '') + (m.columns[i].sections[j].items[k].url !== undefined ? ' href="' + m.columns[i].sections[j].items[k].url + '"' : '') + '><span class="name ellipsis">' + m.columns[i].sections[j].items[k].title + '</span>' + (m.columns[i].sections[j].items[k].subtitle !== undefined ? '<span class="subtitle">' + m.columns[i].sections[j].items[k].subtitle + '</span>' : '') + '</a></li>';
 						}
 					}
 					html += '</ul>';
@@ -209,4 +245,6 @@ function appendMenu(m) {
 // add the custom menu to the menubar
 // if you wanted to add more menus, define another menu structure like customMenu and call appendMenu() with it as a parameter (menus would be added in the order that the appendMenu() calls occur)
 setUserClass();
-appendMenu(customMenu);
+if (userClass != USER_CLASS_NO_MENU) {
+	appendMenu(customMenu);
+}
