cfe4662b7b56cc6fb5a94c21b615675923cf892d
Seth Battis <SethBattis@stmarksschool.org>
2013-08-27 20:25:20 -0400

    Safer Function Naming Pattern
    I realized that just tossing my arbitrarily named Javascript functions into the same namespace as the app that I'm modifying… could end badly.
    
    Now all of my functions are named stmarks_*.
    
    'would be good to move my variables out of that namespace as well, but so far nothing is breaking!
    
    Signed-off-by: Seth Battis <SethBattis@stmarksschool.org>

--- www/javascript/global.js
+++ www/javascript/global.js
@@ -1,26 +1,32 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true */
 
-// modeled on http://stackoverflow.com/a/984656, which I found to be unreliable
-var scripts = document.getElementsByTagName('script');
-var pathToScripts;
-for (var i = 0; i < scripts.length; i++) {
-	if (scripts[i].src.indexOf('stmarksschool.org') != -1) {
-		pathToScripts = scripts[i].src.substr(0, scripts[i].src.indexOf('global.js'));
+function stmarks_globalJavascript() {
+	// modeled on http://stackoverflow.com/a/984656, which I found to be unreliable
+	var i;
+	var scripts = document.getElementsByTagName('script');
+	var pathToScripts;
+	for (i = 0; i < scripts.length; i++) {
+		if (scripts[i].src.indexOf('stmarksschool.org') != -1) {
+			pathToScripts = scripts[i].src.substr(0, scripts[i].src.indexOf('global.js'));
+			break;
+		}
 	}
+	
+	// Works, so long as Canvas includs JQuery ahead of us...
+	// http://stackoverflow.com/a/950146
+	$.getScript(
+		pathToScripts + 'hide-future-courses.js',
+		function() {
+			stmarks_hideFutureCourses();
+		}
+	);
+	
+	$.getScript(
+		pathToScripts + 'resources-menu.js',
+		function() {
+			stmarks_resourcesMenu();
+		}
+	);	
 }
 
-// Works, so long as Canvas includs JQuery ahead of us...
-// http://stackoverflow.com/a/950146
-$.getScript(
-	pathToScripts + 'hide-future-courses.js',
-	function() {
-		hideFutureCourses();
-	}
-);
-
-$.getScript(
-	pathToScripts + 'resources-menu.js',
-	function() {
-		resourcesMenu();
-	}
-);
+stmarks_globalJavascript();
--- www/javascript/hide-future-courses.js
+++ www/javascript/hide-future-courses.js
@@ -1,6 +1,10 @@
-function hideFutureCourses() {
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true */
+
+function stmarks_hideFutureCourses() {
 	var i;
 	var coursesMenu = document.getElementById('menu_enrollments').childNodes[3].childNodes;
+	
+	// TODO: isFaculty is useful enough to warrant a separate file, no?
 	var isFaculty = false;
 	for (i = 1; i < coursesMenu.length; i += 2) {
 		if (coursesMenu[i] instanceof HTMLLIElement && coursesMenu[i].getAttribute('data-id') === '97') {
