48a2087eeaedb7c40e87bcfe57977625f4213bdf
Seth Battis <SethBattis@stmarksschool.org>
2013-09-02 01:06:19 -0400

    Course IDs
    Boy, it would sure be handy, if someone wanted to generate a CSS override to color-code courses, for someone to know what the course IDs for all of our courses in a given term were.
    
    Signed-off-by: Seth Battis <SethBattis@stmarksschool.org>

---
+++ www/api/scripts/courses-in-term-with-id.php
@@ -0,0 +1,40 @@
+<html>
+<body>
+<pre>
+<?php
+
+define ('TOOL_NAME', "Courses in Term {$_REQUEST['enrollment_term_id']} with IDs");
+
+require_once('../.ignore.stmarksschool-test-authentication.inc.php');
+require_once('../debug.inc.php');
+require_once('../canvas-api.inc.php');
+
+debugFlag('START');
+
+$courses = callCanvasApiPaginated(
+	CANVAS_API_GET,
+	'/accounts/1/courses',
+	array(
+		'enrollment_term_id' => $_REQUEST['enrollment_term_id']
+	)
+);
+$page = 1;
+
+echo "id\tsis_course_id\tshort_name\tlong_name\taccount_id\tterm_id" . PHP_EOL;
+
+do {
+	$pageProgress = 'processing page ' . getCanvasApiCurrentPageNumber() . ' of ' . getCanvasApiLastPageNumber() . '...';
+	debugFlag($pageProgress);
+		
+	foreach ($courses as $course) {
+		echo "{$course['id']}\t{$course['sis_course_id']}\t{$course['course_code']}\t{$course['name']}\t{$course['account_id']}" . PHP_EOL;
+	}
+	flush();
+} while ($courses = callCanvasApiNextPage());
+
+debugFlag('FINISH');
+
+?>
+</pre>
+</body>
+</html>
