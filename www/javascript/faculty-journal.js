41358214d69aeec31a9e1195e7d5fd3d8fd4d934
Seth Battis <seth@battis.net>
2013-09-26 16:41:48 -0400

    Variable Timing
    For whatever reason, loading the menu seems to happen slightly sooner than adding the button, which means that the userClass may not have been set yet. Since students can't see the Faculty Journal anyway, no need to check if they're teachers, I guess.
    
    Signed-off-by: Seth Battis <seth@battis.net>

--- www/javascript/faculty-journal.js
+++ www/javascript/faculty-journal.js
@@ -1,7 +1,10 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true */
 
+var courseUsersUrl = /.*\/courses\/(\d+)\/users/;
+var facultyJournalUrl = /.*\/users\/(\d+)\/user_notes\?course_id=(\d+)/;
+var delay = 250;
+
 function stmarks_addFacultyJournalButton() {
-	var courseUsersUrl = /.*\/courses\/(\d+)\/users/;
 	var courseId = courseUsersUrl.exec(document.location.href)[1];
 	var userId = document.getElementsByClassName('StudentEnrollment')[0].id.substr(5);
 	var rightSideToolbar = document.getElementById('right-side').children[0];
@@ -12,27 +15,26 @@ function stmarks_addFacultyJournalButton() {
 	rightSideToolbar.appendChild(facultyJournalLink);
 }
 
-function stmarks_facultyJournal() {
-	if (userClass == USER_CLASS_FACULTY) {
-		// FIXME: this should have some sort of authentication or hash check to prevent, um... abuse
-		var facultyJournalUrl = /.*\/users\/(\d+)\/user_notes\?course_id=(\d+)/;
-		var courseUsersUrl = /.*\/courses\/(\d+)\/users/;
-		
-		// if we're looking at a faculty journal page, insert the list of students
-		if (facultyJournalUrl.test(document.location.href)) {
-			var userId = facultyJournalUrl.exec(document.location.href)[1];
-			var courseId = facultyJournalUrl.exec(document.location.href)[2];
-			var courseMenu = document.createElement('iframe');
-			courseMenu.width = '100%';
-			courseMenu.height = '30';
-			courseMenu.frameBorder = '0';
-			courseMenu.src = 'http://area51.stmarksschool.org/project/canvas/dev/api/faculty-journal/menu.php?course_id=' + courseId + '&user_id=' + userId;
-			var contentDiv = document.getElementById('content');
-			contentDiv.insertBefore(courseMenu, contentDiv.firstChild);
+function stmarks_addFacultyJournalMenu() {
+	var userId = facultyJournalUrl.exec(document.location.href)[1];
+	var courseId = facultyJournalUrl.exec(document.location.href)[2];
+	var courseMenu = document.createElement('iframe');
+	courseMenu.width = '100%';
+	courseMenu.height = '30';
+	courseMenu.frameBorder = '0';
+	courseMenu.src = 'http://area51.stmarksschool.org/project/canvas/dev/api/faculty-journal/menu.php?course_id=' + courseId + '&user_id=' + userId;
+	var contentDiv = document.getElementById('content');
+	contentDiv.insertBefore(courseMenu, contentDiv.firstChild);
+}
 
-		// if we're looking at the list of students, link to the faculty journal page
-		} else if (courseUsersUrl.test(document.location.href)) {
-			window.setTimeout(stmarks_addFacultyJournalButton, 1000)
-		}
+function stmarks_facultyJournal() {
+	// FIXME: this should have some sort of authentication or hash check to prevent, um... abuse
+	
+	// if we're looking at a faculty journal page, insert the list of students
+	if (facultyJournalUrl.test(document.location.href)) {
+		window.setTimeout(stmarks_addFacultyJournalMenu, delay);
+	// if we're looking at the list of students, link to the faculty journal page
+	} else if (userClass == USER_CLASS_FACULTY && courseUsersUrl.test(document.location.href)) {
+		window.setTimeout(stmarks_addFacultyJournalButton, delay * 4);
 	}
 }
