05c6b070ce154be11ef99860372964e6bc023025
Seth Battis <SethBattis@stmarksschool.org>
2013-10-23 16:52:19 -0400

    Oh. There's a checkbox for that.
    Turns out that one can hide future courses from students under account settings. Who knew? Certainly not I.
    
    Drastically simplified this code to make it only about hiding "future past" enrollments (which are stupid and shouldn't be shown anyway.
    
    Signed-off-by: Seth Battis <SethBattis@stmarksschool.org>

--- www/javascript/hide-future-courses.js
+++ www/javascript/hide-future-courses.js
@@ -1,31 +1,11 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true */
 
 function stmarks_hideFutureCourses() {
-	var i, futureEnrollments;
+	var i, futurePastEnrollments;
 
-	// if we don't have userClass or futureEnrollemnts yet, retry soon!
-	if (userClass === undefined || document.getElementsByClassName('future_enrollments').length == 0) {
-		window.setTimeout(stmarks_hideFutureCourses, 25);
-		return;
-	}
-	
-	futureEnrollments = document.getElementsByClassName('future_enrollments')[0];
-		
-	// remove past courses from future list for faculty
-	if (userClass == USER_CLASS_FACULTY) {
-		var futurePastEnrollments = futureEnrollments.querySelectorAll('.completed');
-		for (i = futurePastEnrollments.length - 1; i >= 0; i--) {
-			futurePastEnrollments[i].parentNode.removeChild(futurePastEnrollments[i]);
-		}
-		
-	// hide all future classes from students
-	} else {
-		futureEnrollments.parentNode.removeChild(futureEnrollments);
-		var futureEnrollmentsHeader = document.getElementsByTagName('h2');
-		for (i = 0; i < futureEnrollmentsHeader.length; i++) {
-			if (futureEnrollmentsHeader[i].innerHTML == 'Future Enrollments') {
-				futureEnrollmentsHeader[i].parentNode.removeChild(futureEnrollmentsHeader[i]);
-			}
-		}
+	// remove past courses from future list
+	futurePastEnrollments = document.getElementsByClassName('future_enrollments')[0].querySelectorAll('.completed');
+	for (i = futurePastEnrollments.length - 1; i >= 0; i--) {
+		futurePastEnrollments[i].parentNode.removeChild(futurePastEnrollments[i]);
 	}
 }
