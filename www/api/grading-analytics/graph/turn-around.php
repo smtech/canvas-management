4194dc1338b2cf1483f4885d7bb9411dbb0e515d
Seth Battis <seth@battis.net>
2013-11-02 11:38:50 -0400

    Grading Turn-Around Averages Graph
    Using [PHPGraphLib](http://www.ebrueggeman.com/phpgraphlib) to generate graphs of the collected data on the fly.
    
    First graph up: a graph of all (non-zero) turn-around time averages with a "course of interest" highlighted in red.
    
    Signed-off-by: Seth Battis <seth@battis.net>

---
+++ www/api/grading-analytics/graph/turn-around.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once('../config.inc.php');
+require_once('../.ignore.grading-analytics-authentication.inc.php');
+require_once('../../mysql.inc.php');
+require_once('../../phpgraphlib.php');
+
+$stats = mysqlQuery("
+	SELECT `course[id]`, `average_grading_turn_around` FROM `course_statistics`
+	WHERE
+		`average_grading_turn_around` > 0
+	GROUP BY
+		`course[id]`
+	ORDER BY
+		`timestamp` DESC
+");
+
+$data = array();
+$total = 0;
+while ($row = $stats->fetch_assoc()) {
+	$data[$row['course[id]']] = (int) $row['average_grading_turn_around'];
+	$total += $row['average_grading_turn_around'];
+}
+asort($data);
+$highlight = $data;
+$data[$_REQUEST['course_id']] = 0;
+while (list($key, $value) = each ($highlight)) {
+	if ($key != $_REQUEST['course_id']) {
+		$highlight[$key] = 0;
+	}
+}
+
+$graph = new PHPGraphLib(1860, 300);
+$graph->addData($data);
+$graph->addData($highlight);
+$graph->setBarColor('gray', 'red');
+$graph->setGoalLine($total / count($data), 'gray', 'dashed');
+$graph->setGoalLine(7, 'lime', 'solid');
+$graph->setGoalLine(14, 'red', 'solid');
+$graph->setXValues(false);
+$graph->createGraph();
+
+?>
