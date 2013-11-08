c606694971500eab9ec7a333ff7d52aac19e6a9a
Seth Battis <SethBattis@stmarksschool.org>
2013-11-08 16:10:46 -0500

    Reorganized Course Summary
    Reorganized course-summary.php using the new createPageSection() function and the page-generator.inc.php updates.
    
    Improved documentation on a f ew sections. Still definitely a work in progress.
    
    Signed-off-by: Seth Battis <SethBattis@stmarksschool.org>

--- www/api/grading-analytics/course-summary.php
+++ www/api/grading-analytics/course-summary.php
@@ -4,6 +4,7 @@ require_once('.ignore.grading-analytics-authentication.inc.php');
 define('TOOL_START_PAGE', 'https://' . parse_url(CANVAS_API_URL, PHP_URL_HOST) . "/courses/{$_REQUEST['course_id']}");
 
 require_once('config.inc.php');
+require_once('common.inc.php');
 require_once('../canvas-api.inc.php');
 require_once('../mysql.inc.php');
 require_once('../page-generator.inc.php');
@@ -11,6 +12,11 @@ require_once('../page-generator.inc.php');
 $course = callCanvasApi(
 	CANVAS_API_GET,
 	"/courses/{$_REQUEST['course_id']}"
+);
+
+$department = callCanvasApi(
+	CANVAS_API_GET,
+	"/accounts/{$course['account_id']}"
 );
 
 $statistics = mysqlQuery("
@@ -23,41 +29,102 @@ $statistics = mysqlQuery("
 ");
 $statistic = $statistics->fetch_assoc();
 
-$introduction = '
-	<h3>' . $course['name'] . '</h3>
-	<p>Data collection for these analytics is done at midnight every night, so statistics do not yet reflect grading done today.</p>
-';
+$introduction = buildPageSection('
+	<style>
+		.dashed-underline {
+			border-bottom: 1px dashed black;
+		}
+		
+		.green-underline {
+			border-bottom: 1px solid lime;
+		}
+		
+		.red-underline {
+			border-bottom: 1px solid red;
+		}
+		
+		.red-bar {
+			color: white;
+			background-color: red;
+			padding: 0 2px;
+		}
+	</style>
+	<h1>' . $course['name'] . '</h1>
+	<p>Data collection for these analytics is done at midnight every night, so statistics do not yet reflect grading done today.</p>', false, 'introduction');
 
-$stats = mysqlQuery("
-	SELECT * FROM `course_statistics`
-		WHERE
-			`average_grading_turn_around` > 0
-		GROUP BY
-			`course[id]`
-		ORDER BY
-			`timestamp` DESC
-");
-$total = 0;
-$divisor = 0;
-while ($row = $stats->fetch_assoc()) {
-	$total += $row['average_grading_turn_around'] * $row['student_count'] * $row['graded_assignment_count'];
-	$divisor += $row['student_count'] * $row['graded_assignment_count'];
-}
-$averageTurnAround = $total / $divisor;
+$turnAroundComparison = buildPageSection('
+	<div class="image-placement">
+		<h4>Overall</h4>
+		<a
+			data-lightbox="turn-around-comparison"
+			title="The average grading turn-around time in &ldquo;' . $course['name'] . '&rdquo; compared to all active courses in the school."
+			href="graph/turn-around-comparison.php?course_id=' . $_REQUEST['course_id'] . '">
+				<img src="graph/turn-around-comparison.php?course_id=' . $_REQUEST['course_id'] . '"  style="width: 100%;" />
+			</a>
+		<p class="caption">&ldquo;' . $course['name'] . '&rdquo; compared to all active courses in the school.</p>
+	</div>
+	
+	<div class="image-placement" style="float: right; width: 40%;">
+		<h4>' . $department['name'] . ' Department</h4>
+		<a
+			data-lightbox="turn-around-comparison"
+			title="The average grading turn-around time in &ldquo;' . $course['name'] . '&rdquo; compared to all active courses in the ' . $department['name'] . ' department."
+			href="graph/turn-around-comparison.php?course_id=' . $_REQUEST['course_id'] . '&department_id=' . $course['account_id'] . '">
+				<img src="graph/turn-around-comparison.php?course_id=' . $_REQUEST['course_id'] . '&department_id=' . $course['account_id'] . '"  style="width: 100%;" />
+			</a>
+		<p class="caption">&ldquo;' . $course['name'] . '&rdquo; compared to all active courses in the ' . $department['name'] . ' department.</p>
+	</div>	
 
-$turnAroundComparison = '
-	<h3>Grading Turn-Around Time</h3>
-	<p>What is the average turn-around time (in days) in this course for an assignment, from due date to posted grade in Canvas? The expectation articulated in the faculty handbook is that daily assignments will be returned within a week (green line) and that major assignments will be returned within two weeks (red line). The expectation is that grades will be posted to Canvas at approximately the same time as they are returned to students.</p>
-	<p>The average turn-around time across all courses (dashed line), weighted by number of graded assignments and students, is ' . round($averageTurnAround, 1) . ' days. In &ldquo;' . $course['name'] . '&rdquo; (red column), the current average is ' . round($statistic['average_grading_turn_around'], 1) . ' days.</p>
-	<img src="graph/turn-around-comparison.php?course_id=' . $_REQUEST['course_id'] . '"  width="100%" />
-';
+	<p class="caption">What is the average turn-around time (in days) in this course for an assignment, from due date to posted grade in Canvas? The expectation articulated in the faculty handbook is that daily assignments will be returned within a week (<span class="green-underline">green line</span>) and that major assignments will be returned within two weeks (<span class="red-underline">red line</span>). The expectation is that grades will be posted to Canvas at approximately the same time as they are returned to students.</p>
+	
+	<p class="caption">The average turn-around time across all courses (<span class="dashed-underline">dashed line</span>), weighted by number of graded assignments and students, is ' . round(averageTurnAround(), 1) . ' days. In &ldquo;' . $course['name'] . '&rdquo; (<span class="red-bar">red column</span>), the current average is ' . round($statistic['average_grading_turn_around'], 1) . ' days.</p>
+	
+	<h4>What can be learned from this information?</h4>
+	
+	<ul>
+		<li>How does this course&rsquo;s grading turn-around time compare to other active courses?</li>
+		
+		<li>Is the turn-around time shorter? This may reflect either more prompt feedback to students or fewer complex assignments. It may also reflect due dates being added to assignments after they have already been posted.</li>
+		
+		<li>Is the turn-around time longer? This may reflect either slower feedback to students, more complex assignments, or a disconnect between when grades are shared with students physically and when they are posted to Canvas.</li>
+	</ul>
+	<br clear="all" />',
+	'Grading Turn-Around Time Comparison',
+	'turn-around-comparison'
+);
 
-$turnAroundHistory = '
-	<h3>Grading Turn-Around History</h3d>
-	<img src="graph/turn-around-history.php?course_id=' . $_REQUEST['course_id'] . '" width="100%" />
-';
+$turnAroundHistory = buildPageSection('<img src="graph/turn-around-history.php?course_id=' . $_REQUEST['course_id'] . '" width="100%" />', 'Grading Turn-Around History', 'turn-around-history');
 
-if (isset($statistic['oldest_ungraded_assignment_url'])) {
+$assignmentCount = buildPageSection('
+	<div class="image-placement">
+		<h4>Overall</h4>
+		<a
+			data-lightbox="assignment-count"
+			title="The number of assignments posted in Canvas for &ldquo;' . $course['name'] . '&rdquo; compared to all active courses in the school."
+			href="graph/assignment-count-comparison.php?course_id=' . $_REQUEST['course_id'] . '">
+				<img src="graph/assignment-count-comparison.php?course_id=' . $_REQUEST['course_id'] . '" width="100%" />
+			</a>
+		<p class="caption">&ldquo;' . $course['name'] . '&rdquo; compared to all active courses in the school.</p>
+	</div>
+
+	<div class="image-placement" style="width: 50%; float: right;">
+		<h4>' . $department['name'] . ' Department</h4>
+		<a
+			data-lightbox="assignment-count"
+			title="The number of assignments posted in Canvas for &ldquo;' . $course['name'] . '&rdquo; compared to all active courses in the ' . $department['name'] . ' department."
+			href="graph/assignment-count-comparison.php?course_id=' . $_REQUEST['course_id'] . '&department_id=' . $course['account_id'] . '">
+				<img src="graph/assignment-count-comparison.php?course_id=' . $_REQUEST['course_id'] . '&department_id=' . $course['account_id'] . '" width="100%" />
+			</a>
+		<p class="caption">&ldquo;' . $course['name'] . '&rdquo; compared to all active courses in the ' . $department['name'] . ' department.</p>
+	</div>
+	
+	<p class="caption">How many assignments have been posted to Canvas for &ldquo;' . $course['name'] . '&rdquo;?
+	
+	<br clear="all" />',
+	'Number of Assignments',
+	'assignment-count');
+
+if (strlen($statistic['oldest_ungraded_assignment_url'])) {
 	preg_match('@.*\/assignments\/(\d+)@', $statistic['oldest_ungraded_assignment_url'], $matches);
 	$assignmentInfo = callCanvasApi(
 		CANVAS_API_GET,
@@ -66,13 +133,11 @@ if (isset($statistic['oldest_ungraded_assignment_url'])) {
 	
 	$oldestDueDate = new DateTime($statistic['oldest_ungraded_assignment_due_date']);
 	$now = new DateTime();
-	$oldestUngradedAssignment = '
-		<h3>Oldest Ungraded Assignment</h3>
-		<p>The oldest assignment (whose due date is past) for which there is no grade (for which a grade is anticipated: i.e. not a zero-point assignment or an ungraded assignment).</p>
-		<p><a target="_blank" href="' . $statistic['oldest_ungraded_assignment_url'] . '">' . $assignmentInfo['name'] . '</a>, due ' . $oldestDueDate->format('l, F j, Y') . ' (' . $now->diff($oldestDueDate)->format('%a') . ' days old)</p>
-	';
+	$oldestUngradedAssignment = buildPageSection('<p><a target="_blank" href="' . $statistic['oldest_ungraded_assignment_url'] . '">' . $assignmentInfo['name'] . '</a>, due ' . $oldestDueDate->format('l, F j, Y') . ' (' . $now->diff($oldestDueDate)->format('%a') . ' days old)</p>
+
+				<p>The oldest assignment (whose due date is past) for which there is no grade (for which a grade is anticipated: i.e. not a zero-point assignment or an ungraded assignment).</p>', 'Oldest Ungraded Assignment', 'oldest-ungraded-assignment');
 }
 
-displayPage($introduction . $turnAroundComparison . $turnAroundHistory . $oldestUngradedAssignment);
+displayPage($introduction . $turnAroundComparison . $turnAroundHistory . (isset($oldestUngradedAssignment) ? $oldestUngradedAssignment : '') . $assignmentCount);
 
 ?>
