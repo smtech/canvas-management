a70b2edae7b5e6cc165258a31577586307a8ae3d
Seth Battis <SethBattis@stmarksschool.org>
2014-01-10 13:29:56 -0500

    Objectified
    Use the (much easier to manage) CanvasApiProcess object to handle nested, paginated API responses.
    
    Signed-off-by: Seth Battis <SethBattis@stmarksschool.org>

--- www/scripts/config.inc.php
+++ www/scripts/config.inc.php
@@ -5,7 +5,6 @@ require_once(APP_PATH . '/.ignore.read-only-authentication.inc.php');
 require_once(APP_PATH . '/include/debug.inc.php');
 require_once(APP_PATH . '/include/canvas-api.inc.php');
 
-define('TOOL_NAME', 'Scripts');
 define('START_PAGE', APP_URL . '/scripts/index.php');
 define('DEBUGGING', DEBUGGING_LOG);
 
