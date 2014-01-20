0d09e5605aedec254ef81a509f30e94ec0f0f35c
Seth Battis <SethBattis@stmarksschool.org>
2014-01-20 13:38:06 -0500

    Objectified
    Rewritten and cleaned up to use the CanvasApiProcess object.
    
    Signed-off-by: Seth Battis <SethBattis@stmarksschool.org>

--- www/scripts/courses-in-term-with-id.php
+++ www/scripts/courses-in-term-with-id.php
@@ -7,28 +7,28 @@ require_once('config.inc.php');
 
 define ('TOOL_NAME', "Courses in Term {$_REQUEST['enrollment_term_id']} with IDs");
 
+$coursesApi = new CanvasApiProcess(CANVAS_API_URL, CANVAS_API_TOKEN);
+
 debugFlag('START');
 
-$courses = callCanvasApiPaginated(
-	CANVAS_API_GET,
+$courses = $coursesApi->get(
 	'/accounts/1/courses',
 	array(
-		'enrollment_term_id' => $_REQUEST['enrollment_term_id']
+		'enrollment_term_id' => $_REQUEST['enrollment_term_id'],
 	)
 );
-$page = 1;
 
-echo "id\tsis_course_id\tshort_name\tlong_name\taccount_id\tterm_id" . PHP_EOL;
+echo "course[id]\tcourse[sis_course_id]\tcourse[short_name]\tcourse[long_name]\tcourse[account_id]\tterm[id]" . PHP_EOL;
 
 do {
-	$pageProgress = 'processing page ' . getCanvasApiCurrentPageNumber() . ' of ' . getCanvasApiLastPageNumber() . '...';
+	$pageProgress = 'processing page ' . $coursesApi->getCurrentPageNumber() . ' of ' . $coursesApi->getLastPageNumber() . '...';
 	debugFlag($pageProgress);
 		
 	foreach ($courses as $course) {
-		echo "{$course['id']}\t{$course['sis_course_id']}\t{$course['course_code']}\t{$course['name']}\t{$course['account_id']}" . PHP_EOL;
+		echo "{$course['id']}\t{$course['sis_course_id']}\t{$course['course_code']}\t{$course['name']}\t{$course['account_id']}\t{$_REQUEST['enrollment_term_id']}" . PHP_EOL;
 	}
 	flush();
-} while ($courses = callCanvasApiNextPage());
+} while ($courses = $coursesApi->nextPage());
 
 debugFlag('FINISH');
 
