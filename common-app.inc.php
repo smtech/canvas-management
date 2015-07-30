683a523cf57e8a4c13c453785c9292711acccd28
Seth Battis <sethbattis@stmarksschool.org>
2015-07-29 22:44:25 -0400

    Root AccountAdmin
    Should probably use php-brac to handle roles, but right now we're just forcing a check to make sure that the user is a root-level AccountAdmin.

--- common-app.inc.php
+++ common-app.inc.php
@@ -1,5 +1,15 @@
 <?php
-	
-/* common functions for your app could go here -- it's automatically included in common.inc.php */
+
+try {
+	$canvasManagement = CanvasManagement::getInstance();
+} catch (CanvasManagement_Exception $e) {
+	if ($e->getCode() === CanvasManagement_Exception::INVALID_USER) {
+		$smarty->assign('content', '<h1>' . $toolProvider->user->fullname . '</h1><p>Only root-level account administrators may access this panel.</p>');
+		$smarty->display();
+		exit;
+	} else {
+		throw $e;
+	}
+}
 
 ?>
