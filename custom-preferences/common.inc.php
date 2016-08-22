4a7327f3fc146dc5270177188ddfb974ecc8cbee
Seth Battis <sethbattis@stmarksschool.org>
2016-08-22 14:05:12 -0400

    CaPiTaLiZaTiOn MaTtErS

--- custom-preferences/common.inc.php
+++ custom-preferences/common.inc.php
@@ -4,6 +4,6 @@ require_once __DIR__ . '/../common.inc.php';
 
 use Battis\ConfigXML;
 
-$customprefs = (new ConfigXML(__DIR__ . '/config.xml'))->newInstanceOf(mysqli::class, '/config/mysql');
+$customPrefs = (new ConfigXML(__DIR__ . '/config.xml'))->newInstanceOf(mysqli::class, '/config/mysql');
 
 $toolbox->smarty_prependTemplateDir(__DIR__ . '/templates', basename(__DIR__));
