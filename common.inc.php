42942849285bbbf3d48704be6c0b214f1e8cb8ab
Seth Battis <sethbattis@stmarksschool.org>
2016-08-26 10:21:37 -0400

    Instance Locatior
    It would be really nice to know for sure which instance of Canvas these scripts think that they're running on. Let's display that. Let's also cache data per-instance, to prevent conflicts and confusion.

--- common.inc.php
+++ common.inc.php
@@ -16,18 +16,28 @@ if (empty($_SESSION[Toolbox::class])) {
 $toolbox =& $_SESSION[Toolbox::class];
 
 /* set the Tool Consumer's instance URL, if present */
-if (empty($_SESSION[CANVAS_INSTANCE_URL]) &&
-    !empty($_SESSION[ToolProvider::class]['canvas']['api_domain'])
-) {
-    $_SESSION[CANVAS_INSTANCE_URL] = 'https://' . $_SESSION[ToolProvider::class]['canvas']['api_domain'];
+if (empty($_SESSION[CANVAS_INSTANCE_URL])) {
+    if (!empty($_SESSION[ToolProvider::class]['canvas']['api_domain'])) {
+        $_SESSION[CANVAS_INSTANCE_URL] = 'https://' . $_SESSION[ToolProvider::class]['canvas']['api_domain'];
+    } else {
+        $_SESSION[CANVAS_INSTANCE_URL] = $toolbox->config('TOOL_CANVAS_API')['url'];
+    }
 }
+
+/* cache per-instance */
+$toolbox->cache_pushKey($_SESSION[CANVAS_INSTANCE_URL]);
 
 /* Configure smarty templating */
 /* FIXME this is sometimes superfluous overhead (e.g. action=config) */
 $toolbox->smarty_prependTemplateDir(__DIR__ . '/templates', basename(__DIR__));
+$toolbox->getSmarty()->addStylesheet(
+    DataUtilities::URLfromPath(__DIR__ . '/css/canvas-management.css'),
+    basename(__DIR__)
+);
 $toolbox->smarty_assign([
     'title' => $toolbox->config('TOOL_NAME'),
     'category' => DataUtilities::titleCase(preg_replace('/[\-_]+/', ' ', basename(__DIR__))),
     'APP_URL' => $toolbox->config('APP_URL'),
+    'CANVAS_INSTANCE_URL' => $_SESSION[CANVAS_INSTANCE_URL],
     'navbarActive' => basename(dirname($_SERVER['REQUEST_URI']))
 ]);
---
+++ templates/footer.tpl
@@ -0,0 +1,5 @@
+<footer class="footer">
+    <div class="alert alert-info">
+        <p>Operating on <code>{$CANVAS_INSTANCE_URL}</code></p>
+    </div>
+</footer>
