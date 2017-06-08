cedcbe63b6197c12a80291bde32de37893450842
Seth Battis <sethbattis@stmarksschool.org>
2017-06-08 08:55:12 -0400

    Further navigation templating
    [This](https://github.com/smtech/canvas-management/issues/23) is also now working for Template Courses. (But geez, CSV is sure persnickety!)

--- courses/template-courses.php
+++ courses/template-courses.php
@@ -52,6 +52,20 @@ switch ($step) {
 
                 /* why nest this, I mean... really? */
                 $source = array('course' => $source);
+
+                /* pull course navigation */
+                $tabs = $toolbox->api_get("courses/$template/tabs", [
+                    'include[]' => 'external'
+                ]);
+                $navigation = [];
+                foreach ($tabs as $tab) {
+                    if ($tab['id'] != 'home' && $tab['id'] != 'settings') {
+                        $navigation[$tab['id']] = $tab;
+                    }
+                }
+                uasort($navigation, function ($left, $right) {
+                    return $left['position'] - $right['position'];
+                });
             }
 
             $courses = DataUtilities::loadCsvToArray('csv');
@@ -67,9 +81,18 @@ switch ($step) {
                 } else {
                     foreach ($courses as $course) {
                         /* duplicate course settings */
-                        $course = $toolbox->api_put("/courses/sis_course_id:{$course['course_id']}", $source);
+                        $course = $toolbox->api_put("courses/sis_course_id:{$course['course_id']}", $source);
 
-                        // TODO  nice to figure out navigation settings copy
+                        /* duplicate course navigation */
+                        foreach ($navigation as $id => $tab) {
+                            $toolbox->api_put(
+                                "courses/{$course['id']}/tabs/$id",
+                                [
+                                    'position' => $tab['position'],
+                                    'visibility' => $tab['visibility']
+                                ]
+                            );
+                        }
 
                         /* duplicate course content */
                         $migration = $toolbox->api_post(
